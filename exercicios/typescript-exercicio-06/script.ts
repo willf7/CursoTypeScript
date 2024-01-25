d2e73876ee62672cfd3f559fa233f8b878c00218
William Felipe <williamfelipe081@gmail.com>
2024-01-25 19:45:44 -0300

    Adicao e resolucao do exercicio 06

---
+++ exercicios/typescript-exercicio-06/script.ts
@@ -0,0 +1,21 @@
+const btn = document.getElementById("origamid");
+
+/* 
+Forma q eu pensei
+
+function changeLink() {
+  if (btn) {
+    let href = btn.getAttribute("href");
+    let hrefAtualizado = href?.replace("http://", "https://");
+    btn.removeAttribute("href");
+    if (hrefAtualizado) {
+      btn.setAttribute("href", hrefAtualizado);
+    }
+  }
+}
+
+changeLink(); */
+
+if (btn instanceof HTMLAnchorElement) {
+  btn.href = btn.href.replace("http://", "https://");
+}
