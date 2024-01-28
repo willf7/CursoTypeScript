6efd15a726bd426c3fe4159eeae37b58a2d47044
William Felipe <williamfelipe081@gmail.com>
2024-01-27 23:25:57 -0300

    Adicao e conclusao da aula 10 (Eventos e Callback)

---
+++ typescript-aula-10/script.ts
@@ -0,0 +1,45 @@
+/* const btn = document.querySelector("button");
+
+function handleClick(event: MouseEvent) {
+  console.log("clickou");
+}
+
+btn?.addEventListener("click", handleClick);
+
+function ativarMenu(event: Event) {
+  if (event instanceof MouseEvent) {
+    console.log("E uma instacia de MouseEvent");
+    console.log(event.pageX);
+  }
+  if (event instanceof KeyboardEvent) {
+    console.log("E uma instacia de KeyboardEvent");
+    console.log(event.key);
+  }
+  if (event instanceof TouchEvent) {
+    console.log("E uma instacia de TouchEvent");
+  }
+}
+
+document.documentElement.addEventListener("mousedown", ativarMenu);
+document.documentElement.addEventListener("touchstart", ativarMenu);
+window.addEventListener("keydown", ativarMenu);
+ */
+
+/* const btn = document.querySelector("button");
+
+function handleClick(this: HTMLButtonElement, event: MouseEvent) {
+  console.log(this);
+}
+
+btn?.addEventListener("click", handleClick); */
+
+const btn = document.querySelector("button");
+
+function handleClick(event: MouseEvent) {
+  const elemento = event.currentTarget;
+  if (elemento instanceof HTMLElement) {
+    console.log(elemento.innerHTML);
+  }
+}
+
+btn?.addEventListener("click", handleClick);
